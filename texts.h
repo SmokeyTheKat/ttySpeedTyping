a25dc20afb559f65a5e04c5ad06545f5c39a0dd8
smokeythekat <owr5273@gmail.com>
2021-05-20 14:38:33 -0300

    python :)

--- texts.h
+++
@@ -1,4 +0,0 @@
-struct text texts[] = {
-	{ "Hello", "Hi there how are you doing? I am doing very good :)" },
-	{ "Father of Britain", "Her father believed it was sensible to live on Vancouver Island, a colony of Great Britain, where he could practice English customs and continue his British citizenship." },
-};
